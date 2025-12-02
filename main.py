0b78406f58fce1ac159d6a6c0ba87045b97fbe72
STDW-09 <stefdewilde09@gmail.com>
2025-12-02 17:36:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -607,3 +607,9 @@ init_db()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # 5000 voor lokaal testen
+    app.run(host="0.0.0.0", port=port)
+    
